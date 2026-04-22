1cc972896fc90f20816f1a6889c09b635a755839
souranshuaryal2029-design <souranshu.aryal2029@ullens.edu.np>
2026-04-22 01:49:39 -0700

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,41 @@
+name = "Bob"
+age = 21
+is_student = True
+weight = 69.5
+
+print("Name :", name)
+print("Data Type of Name is", type(name))
+print("Age :", age)
+print("Data Type of Age is", type(age))
+print("I think you are a student", is_student)
+print("Data Type of Is_student is", type(is_student))
+print("Weight :", weight)
+print("Data Type of Weight is", type(weight))
+weight = int(weight)
+print("Weight :", weight)
+print("Data Type of Weight is", type(weight))
+
+
+x = 50
+y = 60
+z = (x+y)
+
+print("The sum of 50 and 60 is", z)
+
+x = 50
+y = 60
+z = (x-y)
+
+print("The subtraction of 50 and 60 is", z)
+
+x = 50
+y = 60
+z = (x/y)
+
+print("The division of 50 and 60 is", z)
+
+x = 50
+y = 60
+z = (x*y)
+
+print("The multiply of 50 and 60 is", z)
