3b4570ac54a8fb57d8e8f54ce57c3538740c386e
souranshuaryal2029-design <souranshu.aryal2029@ullens.edu.np>
2026-04-29 23:53:49 -0700

    Add files via upload

---
+++ Absfun.py
@@ -0,0 +1,11 @@
+def absolute_value(num):
+
+    if num >=0 :
+        return num
+    else:
+        return -num
+    
+
+print("Absolute value of 89 :", absolute_value(89))
+
+print("Absolute value of -189 :", absolute_value(-189))
