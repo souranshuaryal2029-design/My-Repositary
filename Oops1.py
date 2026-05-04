d8b984fd7f876f21f54b57e0a46bd2ba13b9a434
souranshuaryal2029-design <souranshu.aryal2029@ullens.edu.np>
2026-05-04 04:01:22 -0700

    Add files via upload

--- Oops1.py
+++ Oops1.py
@@ -1,16 +1,23 @@
 class dinosaurs:
 
-
     species = "Giganotasaurus"
-
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    
+    def sing(self, song):
+        return "{} sings {}".format(self.name, song)
+    
+    def dance(self):
+        return "{} is now dancing".format(self.name)
 
 
 fatpig = dinosaurs("fatpig", 21)
 skinnypig = dinosaurs("skinnypig", 30)
+
+print(fatpig.sing("Fried chicken"))
+print(skinnypig.dance())
 
 
 print("Fatpig is a {}".format(fatpig.species))
