7b2a4da8f9b3f5bf5e1c208922d4f315cddda617
souranshuaryal2029-design <souranshu.aryal2029@ullens.edu.np>
2026-05-14 03:06:42 -0700

    Add files via upload

---
+++ Mytuples2.py
@@ -0,0 +1,18 @@
+
+my_tuple = ()
+print(my_tuple)
+
+
+my_tuple = (2, 1, 6)
+print(my_tuple)
+
+
+my_tuple = (1, "Welcome Back", 5.4)
+print(my_tuple)
+
+
+my_tuple = ("mouse", [6, 9, 4], [2, 1, 6])
+print(my_tuple[1][1])
+
+
+print(my_tuple[1][1])
