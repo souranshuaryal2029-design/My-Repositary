93ef03742dab5769da7d49c7e9a5f9ac83658746
souranshuaryal2029-design <souranshu.aryal2029@ullens.edu.np>
2026-05-28 03:59:15 -0700

    Add files via upload

---
+++ removeline.py
@@ -0,0 +1,11 @@
+file1 = open('Codingal.txt', 'r')
+
+file2 = open('CodingalUpdated.txt', 'w')
+
+for line in file1.readlines():
+    if not(line.startswith('Shreyansh')):
+        print(line)
+        file2.write(line)
+
+file1.close()
+file2.close()
