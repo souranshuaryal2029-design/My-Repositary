c17d4f46ffa36f4bb3c42fab33ea68a92f519b43
souranshuaryal2029-design <souranshu.aryal2029@ullens.edu.np>
2026-06-05 05:57:58 -0700

    Add files via upload

---
+++ Naturalnumber(after class).py
@@ -0,0 +1,8 @@
+num = int(input("Enter a Number"))
+
+if num>0:
+    print(num ,"is a natural number")
+else:
+    print(num ,"is not natural a number")
+if num == 0:
+    print(num ,"is a neutral number")
